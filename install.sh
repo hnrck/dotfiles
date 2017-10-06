4966631a3c145e733ce5e955fae47a03f307fea7
Henrick Deschamps <henrick.deschamps@gmail.com>
2017-10-06 22:18:20 +0200

    Adding sakura conf, and a simple bash installer.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash --
+
+# Constants
+DOT_CONF=~/.config/
+
+# Tools
+DATE=`date +%Y-%m-%d-%H:%M:%S`
+
+# Sakura
+SAKURA_DIR=$DOT_CONF/sakura
+SAKURA_CONF=$SAKURA_DIR/sakura.conf
+mkdir -p $SAKURA_DIR
+if [ -e $SAKURA_CONF ]
+then
+    mv $SAKURA_CONF $SAKURA_CONF.$DATE.old
+fi
+if [ -L $SAKURA_CONF ]
+then
+    unlink  $SAKURA_CONF
+fi
+ln ./sakura/sakura.conf $SAKURA_DIR
