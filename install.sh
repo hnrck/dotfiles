065cc7ddaee6486088e7d8aa8fdf289a9a24714c
Henrick Deschamps <henrick.deschamps@gmail.com>
2018-03-04 07:01:33 +0100

    Improving installation process

--- install.sh
+++ install.sh
@@ -1,21 +1,7 @@
 #!/bin/bash --
 
-# Constants
-DOT_CONF=~/.config/
-
-# Tools
-DATE=`date +%Y-%m-%d-%H:%M:%S`
-
 # Sakura
-SAKURA_DIR=$DOT_CONF/sakura
-SAKURA_CONF=$SAKURA_DIR/sakura.conf
-mkdir -p $SAKURA_DIR
-if [ -e $SAKURA_CONF ]
-then
-    mv $SAKURA_CONF $SAKURA_CONF.$DATE.old
-fi
-if [ -L $SAKURA_CONF ]
-then
-    unlink  $SAKURA_CONF
-fi
-ln ./sakura/sakura.conf $SAKURA_DIR
+bash ./sakura/sakura-install.sh
+
+# Github
+bash ./github/github-clone.sh
