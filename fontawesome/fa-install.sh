0c74465ff6561dfe69e2cd7c239889e92b8416d0
Henrick Deschamps <henrick.deschamps@gmail.com>
2018-03-15 20:02:15 +0100

    Correcting fontawesome installation

--- fontawesome/fa-install.sh
+++ fontawesome/fa-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash --
 
 TMP_DIR=tmp_dir_to_remove/
+FONTS_DIR=$HOME/.fonts/
 
 cd fontawesome/
 
@@ -9,15 +10,15 @@ cd $TMP_DIR
 
 wget -c https://use.fontawesome.com/releases/v5.0.8/fontawesome-free-5.0.8.zip
 
-unzip fontawesome-free-5.0.8.zip
+unzip -qq fontawesome-free-5.0.8.zip
 
-if [ ! -e ~/.fonts ]
+if [ ! -e $FONTS_DIR ]
 then
-    cp fontawesome-free-5.0.8/use-on-desktop/*.otf ~/.fonts
+    mkdir $FONTS_DIR
 fi
 
+cp fontawesome-free-5.0.8/use-on-desktop/*.otf $FONTS_DIR
+
 cd ..
-
 rm -rf $TMP_DIR
-
 cd ..
