aa4e1298ee8aea98ac3e8d8f5041cd3252277db3
Henrick Deschamps <henrick.deschamps@gmail.com>
2018-07-14 16:31:55 +0200

    Modifying install script

--- install.sh
+++ install.sh
@@ -1,28 +1,47 @@
 #!/bin/bash --
 
-# ZSH confs
-bash zsh/zsh-install.sh
+# Set VERBOSE to 1 for info during installation, else 0
+export VERBOSE=1
 
-# Git
-bash git/git-install.sh
+# Confs to install
+CONFS=(
+  # ZSH confs
+  zsh/zsh-install.sh
 
-# NeoVim
-bash neovim/neovim-install.sh
+  # Git
+  git/git-install.sh
 
-# Sakura conf
-bash sakura/sakura-install.sh
+  # NeoVim
+  neovim/neovim-install.sh
 
-# Clang
-bash clang/clang-install.sh
+  # Sakura conf
+  sakura/sakura-install.sh
 
-# Stack
-bash stack/stack-install.sh
+  # Clang
+  clang/clang-install.sh
 
-# XMonad
-bash xmonad/xmonad-install.sh
+  # Stack
+  stack/stack-install.sh
 
-# Font Awesome
-bash fontawesome/fa-install.sh
+  # XMonad
+  xmonad/xmonad-install.sh
 
-# Github
-bash github/github-clone.sh
+  # Font Awesome
+  fontawesome/fa-install.sh
+
+  # Github
+  github/github-clone.sh
+)
+
+for CONF in ${CONFS[@]}
+do
+  if [ ${VERBOSE} -eq 1 ]
+  then 
+    echo -e "file: ${CONF}"
+  fi
+  bash ${CONF}
+  if [ ${VERBOSE} -eq 1 ]
+  then 
+    echo -e ""
+  fi
+done
