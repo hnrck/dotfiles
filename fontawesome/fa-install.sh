5d47d3a5b243c3006e75eb254dfa23b3a50ec5fa
Henrick Deschamps <henrick.deschamps@gmail.com>
2018-07-18 22:00:25 +0200

    Modifying fontawesome install script

--- fontawesome/fa-install.sh
+++ fontawesome/fa-install.sh
@@ -1,24 +1,38 @@
 #!/bin/bash --
 
+FONTAWESOME_VERSION=5.0.8
+FONTAWESOME=fontawesome-free-${FONTAWESOME_VERSION}
+
+FONTS_DIR=${HOME}/.fonts
+
 TMP_DIR=tmp_dir_to_remove/
 FONTS_DIR=$HOME/.fonts/
 
 cd fontawesome/
 
-mkdir $TMP_DIR
-cd $TMP_DIR
-
-wget -c https://use.fontawesome.com/releases/v5.0.8/fontawesome-free-5.0.8.zip
-
-unzip -qq fontawesome-free-5.0.8.zip
-
-if [ ! -e $FONTS_DIR ]
+if [ -e "${FONTS_DIR}/Font Awesome 5 Brands-Regular-400.otf" ] \
+  && [ -e "${FONTS_DIR}/Font Awesome 5 Free-Regular-400.otf" ] \
+  && [ -e "${FONTS_DIR}/Font Awesome 5 Free-Solid-900.otf" ]
 then
-    mkdir $FONTS_DIR
+  exit
 fi
 
-cp fontawesome-free-5.0.8/use-on-desktop/*.otf $FONTS_DIR
+mkdir ${TMP_DIR}
+cd ${TMP_DIR}
+
+wget -c https://use.fontawesome.com/releases/v${FONTAWESOME_VERSION}/${FONTAWESOME}.zip
+
+unzip ${FONTAWESOME}.zip
+
+if [ ! -e ${FONTS_DIR} ]
+then
+  mkdir -p ${FONTS_DIR}
+fi
+
+cp fontawesome-free-5.0.8/use-on-desktop/*.otf ${FONTS_DIR}
 
 cd ..
-rm -rf $TMP_DIR
+
+rm -rf --preserve-root -- ${TMP_DIR}
+
 cd ..
